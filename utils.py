72ae06f6ce8086c4d0a6a2a646e933a8511dff1d
Kamil Burdzinski <kburdzinski@internships.antmicro.com>
2020-06-11 14:44:34 +0200

    Fix runner loading

--- utils.py
+++ utils.py
@@ -4,14 +4,15 @@ import os
 def load_runners():
     paths = ['pacman_layouts/' + f for f in os.listdir('pacman_layouts')]
     random_ghosts = [False, True]
-    
-    runners = []
+
+    runners, names = [], []
     for p in paths:
         for rg in random_ghosts:
             runner = LocalPacmanGameRunner(layout_path=p,
                                            random_ghosts=rg,
                                            frame_time=0)
             runners.append(runner)
-    names = [p.split('/')[1] for p in paths]
+            names.append(p.split('/')[1] + f'__random_ghosts={rg}')
+    assert len(runners) == len(names)
     return runners, names
 
