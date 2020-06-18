a2b92945882e143ad52313a3c4c62d8aa6026a97
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-06-18 18:17:20 +0200

    Dist features

--- fextractor.py
+++ fextractor.py
@@ -36,7 +36,6 @@ capsule_west
 '''
 
 class Extractor():
-
     def __call__(self, state):
         ghosts, g_states = state.getGhostPositions(), state.getGhostStates()
         food = state.getFood()
@@ -45,30 +44,28 @@ class Extractor():
         x, y = state.getPacmanPosition()
         parsed = torch.zeros((4,3), dtype=torch.float32)
 
-        # ghosts [0-3]
+        # ghosts [0th column]
         for g, s in zip(ghosts, g_states):
-            dist = abs(g[0]-x) + abs(g[1]-y)
-            v = -1
-            if s.scaredTimer != 0:
-                v = 1
-            if dist <= 2:
-                if g[1] > y:
-                    parsed[0][0] = v
-                if g[0] > x:
-                    parsed[1][0] = v
-                if y > g[1]:
-                    parsed[2][0] = v
-                if x > g[0]:
-                    parsed[3][0] = v
+            dist = abs(x-g[0])+abs(y-g[1])
+            if s.scaredTimer == 0 and dist <= 2:
+                v = -1
+            elif s.scaredTimer != 0:
+                dist = cell_dist((x,y), g, walls)
+                v = (s.scaredTimer-dist)/40
+            else:
+                v = 0
 
-        # walls [4-7]
-#        for idx, (dx, dy) in enumerate([(0,1), (1,0), (0,-1), (-1,0)]):
-#            if walls[x+dx][y+dy]:
-#                parsed[idx+4] = 1
+            if g[1] > y:
+                parsed[0][0] = v
+            if g[0] > x:
+                parsed[1][0] = v
+            if y > g[1]:
+                parsed[2][0] = v
+            if x > g[0]:
+                parsed[3][0] = v
 
-        # food position [8-11]
-        for idx in range(4):
-            parsed[idx][1] = 1     #max dist
+        # food distance [1st column]
+        parsed[:,1] = 1     #max dist
         legals = state.getLegalActions()
         if Directions.NORTH in legals:
             parsed[0][1] = closest_food((x,y+1), food, walls) / (food.width*food.height)
@@ -79,18 +76,17 @@ class Extractor():
         if Directions.WEST in legals:
             parsed[3][1] = closest_food((x-1,y), food, walls) / (food.width*food.height)
 
-        # closest capsule position [12-15]
+        # capsule distance [2nd column]
         for c in capsules:
-            dist = abs(c[0]-x) + abs(c[1]-y)
-            if dist <= 2:
-                if c[1] > y:
-                    parsed[0][2] = 1
-                if c[0] > x:
-                    parsed[1][2] = 1
-                if y > c[1]:
-                    parsed[2][2] = 1
-                if x > c[0]:
-                    parsed[3][2] = 1
+            dist = cell_dist((x,y), c, walls) / (walls.width * walls.height)
+            if c[1] > y:
+                parsed[0][2] = dist
+            if c[0] > x:
+                parsed[1][2] = dist
+            if y > c[1]:
+                parsed[2][2] = dist
+            if x > c[0]:
+                parsed[3][2] = dist
 
         return parsed.cuda()
 
@@ -115,6 +111,21 @@ def closest_food(pos, food, walls):
             fringe.append((nbr_x, nbr_y, dist + 1))
     # no food found
     return None
+
+def cell_dist(pos, cell, walls):
+    fringe = [(pos[0], pos[1], 0)]
+    expanded = set()
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        if pos_x == cell[0] and pos_y == cell[1]: 
+            return dist
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist + 1))
+    raise Exception("Cell not found!")
 
 
 
